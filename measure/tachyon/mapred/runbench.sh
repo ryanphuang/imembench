19131198b30bf68c22e9b263d3768eaccb28f544
Tianyin Xu <xuty1985@gmail.com>
2015-06-04 18:30:41 -0700

    remove the HDFS part

--- measure/tachyon/mapred/runbench.sh
+++ measure/tachyon/mapred/runbench.sh
@@ -1,31 +1,22 @@
 #!/bin/bash
 
+#TODO
 FILE10=big.txt
 
 ###TODO: export
-HDFS_MASTER=ccied6.sysnet.ucsd.edu
+TACHYON_HOME=/mnt/tachyon/tachyon-0.6.4
 TACHYON_MASTER=ccied6.sysnet.ucsd.edu
 
 USER=`whoami`
 PREFIX=/user/$USER
 
-HDFS_PREFIX=hdfs://$TACHYON_MASTER:9000$PREFIX
 TACHYON_PREFIX=tachyon://$TACHYON_MASTER:19998$PREFIX
-
-#$HADOOP_HOME/bin/hadoop dfs -rm -r $WC_INPUT
-#$HADOOP_HOME/bin/hadoop dfs -copyFromLocal XXX $WC_INPUT/big.txt 
-
-HADOOP_HOME=/mnt/hadoop/hadoop-2.5.2
-TACHYON_HOME=/mnt/tachyon/tachyon-0.6.4
 
 if [ $# -ne 1 ]
 then
     echo "please specify the workload to run (wordcount|grep|terasort)'"
 else
-    
     WORKLOAD=$1
-    HDFS_OUTPUT=$HDFS_PREFIX/$WORKLOAD"_output"
-    HDFS_INPUT_DIR=$HDFS_PREFIX/$WORKLOAD"_input"
     
     TACHYON_OUTPUT=$TACHYON_PREFIX/$WORKLOAD"_output"
     TACHYON_INPUT_DIR=$TACHYON_PREFIX/$WORKLOAD"_input"
@@ -53,10 +44,5 @@ else
     else
         echo "Sorry we do not support this benchmark ("$WORKLOAD")"
     fi
+    $TACHYON_HOME/bin/tachyon tfs rmr $TACHYON_OUTPUT
 fi
-
-###USING HDFS
-#$HADOOP_HOME/bin/hdfs dfs -rm -r $WC_HDFS_OUTPUT
-#$HADOOP_HOME/bin/hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.5.2.jar wordcount $WC_HDFS_INPUT_DIR/$FILE10 $WC_HDFS_OUTPUT
-
-
