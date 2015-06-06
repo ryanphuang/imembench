0dbbc2db80e28ee234d8839c5ea4b6517ef5c729
Tianyin Xu <xuty1985@gmail.com>
2015-06-06 16:40:57 -0700

    integrate ICS Hadoop benchmarks in our measurement which contains workloads for wordcount, grep, and sort (tested on 10G data)

--- measure/tachyon/mapred/runbench_hdfs.sh
+++ measure/tachyon/mapred/runbench_hdfs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#TODO
-FILE10=big.txt
+#TODO: redundancy here
 
 ###TODO: export
 HADOOP_HOME=/mnt/hadoop/hadoop-2.5.2
@@ -9,41 +8,41 @@ HADOOP_HOME=/mnt/hadoop/hadoop-2.5.2
 HDFS_MASTER=ccied6.sysnet.ucsd.edu
 
 USER=`whoami`
-PREFIX=/user/$USER
+PREFIX=/user/$USER/data-MicroBenchmarks
+
+RAW_FILES=$PREFIX/in
+SORT_FILES=$PREFIX/sort-out
+HDFS_OUTPUT=$PREFIX/out
 
 HDFS_PREFIX=hdfs://$HDFS_MASTER:9000$PREFIX
 
 if [ $# -ne 1 ]
 then
-    echo "please specify the workload to run (wordcount|grep|terasort)'"
+    echo "please specify the workload to run (wordcount|grep|sort|terasort)'"
 else
-    WORKLOAD=$1
-   
-    HDFS_OUTPUT=$HDFS_PREFIX/$WORKLOAD"_output"
-    HDFS_INPUT_DIR=$HDFS_PREFIX/$WORKLOAD"_input"
+    if [ $1 == "wordcount" ];then 
+        echo "START RUN "$1
+        $HADOOP_HOME/bin/hadoop fs -rm -r $HDFS_OUTPUT/wordcount
+        $HADOOP_HOME/bin/hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.5.2.jar wordcount $RAW_FILES $HDFS_OUTPUT/wordcount
 
-    echo $HDFS_OUTPUT  
-    $HADOOP_HOME/bin/hdfs dfs -rm -r $HDFS_OUTPUT
+    elif [ $1 == "grep" ]; then
+        echo "START RUN "$1
+        $HADOOP_HOME/bin/hadoop fs -rm -r $HDFS_OUTPUT/grep
+        $HADOOP_HOME/bin/hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.5.2.jar grep $RAW_FILES $HDFS_OUTPUT/grep "dfs[a-z.]+"
     
-    if [ $WORKLOAD == "wordcount" ]
-    then
+    elif [ $1 == "sort" ]; then
         echo "START RUN "$1
-        $HADOOP_HOME/bin/hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.5.2.jar wordcount $HDFS_INPUT_DIR/$FILE10 $HDFS_OUTPUT
+        $HADOOP_HOME/bin/hadoop fs -rm -r $HDFS_OUTPUT/sort
+        $HADOOP_HOME/bin/hadoop jar ${HADOOP_HOME}/share/hadoop/mapreduce/hadoop-mapreduce-examples-*.jar sort $SORT_FILES $HDFS_OUTPUT/sort
 
-    elif [ $WORKLOAD == "grep" ]
-    then
-        echo "START RUN "$1
-        $HADOOP_HOME/bin/hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.5.2.jar grep $HDFS_INPUT_DIR/$FILE10 $HDFS_OUTPUT "dfs[a-z.]+"
-
-    elif [ $WORKLOAD == "terasort" ]
-    then
+    elif [ $1 == "terasort" ]; then
         echo "START RUN "$1
         TSORT_FILE="tsort_data"
         $HADOOP_HOME/bin/hdfs dfs -rm $HDFS_INPUT_DIR/$TSORT_FILE
         $HADOOP_HOME/bin/hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.5.2.jar teragen 1000 $HDFS_INPUT_DIR/$TSORT_FILE
         $HADOOP_HOME/bin/hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.5.2.jar terasort $HDFS_INPUT_DIR/$TSORT_FILE $HDFS_OUTPUT
     else
-        echo "Sorry we do not support this benchmark ("$WORKLOAD")"
+        echo "Sorry we do not support this benchmark ("$1")"
     fi
-    $HADOOP_HOME/bin/hdfs dfs -rm -r $HDFS_OUTPUT
+    #$HADOOP_HOME/bin/hdfs dfs -rm -r $HDFS_OUTPUT
 fi
--- measure/tachyon/mapred/runbench_tachyon.sh
+++ measure/tachyon/mapred/runbench_tachyon.sh
@@ -1,49 +1,47 @@
 #!/bin/bash
 
-#TODO
-FILE10=big.txt
-
 ###TODO: export
 TACHYON_HOME=/mnt/tachyon/tachyon-0.6.4
 
 TACHYON_MASTER=ccied6.sysnet.ucsd.edu
 
 USER=`whoami`
-PREFIX=/user/$USER
+PREFIX=/user/$USER/data-MicroBenchmarks
 
 TACHYON_PREFIX=tachyon://$TACHYON_MASTER:19998$PREFIX
 
+RAW_FILES=$TACHYON_PREFIX/in
+SORT_FILES=$TACHYON_PREFIX/sort-out
+TACHYON_OUTPUT=$TACHYON_PREFIX/out
+
 if [ $# -ne 1 ]
 then
-    echo "please specify the workload to run (wordcount|grep|terasort)'"
+    echo "please specify the workload to run (wordcount|grep|sort|terasort)'"
 else
-    WORKLOAD=$1
-    
-    TACHYON_OUTPUT=$TACHYON_PREFIX/$WORKLOAD"_output"
-    TACHYON_INPUT_DIR=$TACHYON_PREFIX/$WORKLOAD"_input"
-
-    $TACHYON_HOME/bin/tachyon tfs rmr $TACHYON_OUTPUT
-    
-    if [ $WORKLOAD == "wordcount" ]
-    then
+    if [ $1 == "wordcount" ];then
         echo "START RUN "$1
-        $HADOOP_HOME/bin/hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.5.2.jar wordcount -libjars $TACHYON_HOME/client/target/tachyon-client-0.6.4-jar-with-dependencies.jar $TACHYON_INPUT_DIR/$FILE10 $TACHYON_OUTPUT
+        $TACHYON_HOME/bin/tachyon tfs rmr $TACHYON_OUTPUT_DIR/wordcount
+        $HADOOP_HOME/bin/hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.5.2.jar wordcount -libjars $TACHYON_HOME/client/target/tachyon-client-0.6.4-jar-with-dependencies.jar $RAW_FILES $TACHYON_OUTPUT/wordcount
 
-    elif [ $WORKLOAD == "grep" ]
-    then
+    elif [ $1 == "grep" ];then
         echo "START RUN "$1
-        $HADOOP_HOME/bin/hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.5.2.jar grep -libjars $TACHYON_HOME/client/target/tachyon-client-0.6.4-jar-with-dependencies.jar $TACHYON_INPUT_DIR/$FILE10 $TACHYON_OUTPUT "dfs[a-z.]+"
+        $TACHYON_HOME/bin/tachyon tfs rmr $TACHYON_OUTPUT_DIR/grep
+        $HADOOP_HOME/bin/hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.5.2.jar grep -libjars $TACHYON_HOME/client/target/tachyon-client-0.6.4-jar-with-dependencies.jar $RAW_FILES $TACHYON_OUTPUT/grep "dfs[a-z.]+"
 
-    elif [ $WORKLOAD == "terasort" ]
-    then
+    elif [ $1 == "sort" ];then
+        echo "START RUN "$1
+        $TACHYON_HOME/bin/tachyon tfs rmr $TACHYON_OUTPUT_DIR/sort
+        $HADOOP_HOME/bin/hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.5.2.jar sort -libjars $TACHYON_HOME/client/target/tachyon-client-0.6.4-jar-with-dependencies.jar $SORT_FILES $TACHYON_OUTPUT/sort
+        
+    elif [ $1 == "terasort" ];then
         echo "START RUN "$1
         TSORT_FILE="tsort_data"
-        $TACHYON_HOME/bin/tachyon tfs rm $TACHYON_INPUT_DIR/$TSORT_FILE
-        $HADOOP_HOME/bin/hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.5.2.jar teragen -libjars $TACHYON_HOME/client/target/tachyon-client-0.6.4-jar-with-dependencies.jar 1000 $TACHYON_INPUT_DIR/$TSORT_FILE
+        $TACHYON_HOME/bin/tachyon tfs rmr $TACHYON_INPUT_DIR/$TSORT_FILE
+        $HADOOP_HOME/bin/hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.5.2.jar teragen -libjars $TACHYON_HOME/client/target/tachyon-client-0.6.4-jar-with-dependencies.jar 1000000 $TACHYON_INPUT_DIR/$TSORT_FILE
         
         $HADOOP_HOME/bin/hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.5.2.jar terasort -libjars $TACHYON_HOME/client/target/tachyon-client-0.6.4-jar-with-dependencies.jar $TACHYON_INPUT_DIR/$TSORT_FILE $TACHYON_OUTPUT
     else
-        echo "Sorry we do not support this benchmark ("$WORKLOAD")"
+        echo "Sorry we do not support this benchmark ("$1")"
     fi
-    $TACHYON_HOME/bin/tachyon tfs rmr $TACHYON_OUTPUT
+    #$TACHYON_HOME/bin/tachyon tfs rmr $TACHYON_OUTPUT
 fi
