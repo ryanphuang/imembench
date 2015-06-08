c060841655ca987d75156dd12c39e56d5cf1c78d
Tianyin Xu <xuty1985@gmail.com>
2015-06-07 21:24:49 -0700

    integrate ICS spark benchmark

---
+++ measure/tachyon/spark/runbench_tachyon.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+TACHYON_HOME=/mnt/tachyon/tachyon-0.6.4
+
+TACHYON_MASTER=ccied6.sysnet.ucsd.edu
+
+USER=`whoami`
+PREFIX=/user/$USER/data-MicroBenchmarks
+
+TACHYON_PREFIX=tachyon://$TACHYON_MASTER:19998$PREFIX
+
+RAW_FILES=$TACHYON_PREFIX/in
+SORT_FILES=$TACHYON_PREFIX/sort-out
+TSORT_FILE=$TACHYON_PREFIX/tsort-file
+TACHYON_OUTPUT=$TACHYON_PREFIX/out
+
+if [ $# -ne 1 ]
+then
+    echo "please specify the workload to run (pagerank|grep|terasort)'"
+else
+    echo "START RUN "$1
+
+    if [ $1 == "wordcount" ];then
+        $TACHYON_HOME/bin/tachyon tfs rmr $TACHYON_OUTPUT/spark-wordcount
+        $SPARK_HOME/bin/spark-submit --class cn.ac.ict.bigdatabench.WordCount bigdatabench-spark_2.10-1.1.0.jar $RAW_FILES $TACHYON_OUTPUT/spark-wordcount
+
+    elif [ $1 == "grep" ];then
+        $TACHYON_HOME/bin/tachyon tfs rmr $TACHYON_OUTPUT/spark-grep
+        $SPARK_HOME/bin/spark-submit --class cn.ac.ict.bigdatabench.Grep bigdatabench-spark_2.10-1.1.0.jar $RAW_FILES henry $TACHYON_OUTPUT/spark-grep
+
+    elif [ $1 == "sort" ];then
+        $TACHYON_HOME/bin/tachyon tfs rmr $TACHYON_OUTPUT/spark-sort
+        $SPARK_HOME/bin/spark-submit --class cn.ac.ict.bigdatabench.Sort bigdatabench-spark_2.10-1.1.0.jar $RAW_FILES $TACHYON_OUTPUT/spark-sort
+
+    else
+        echo "Sorry we do not support this benchmark ("$WORKLOAD")"
+    fi
+    #$TACHYON_HOME/bin/tachyon tfs rmr $TACHYON_OUTPUT
+fi
