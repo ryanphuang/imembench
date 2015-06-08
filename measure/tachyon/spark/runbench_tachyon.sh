51f35f48f15e4ae3faff08acc5bf1a35b92c5246
Tianyin Xu <xuty1985@gmail.com>
2015-06-08 13:20:15 -0700

    let spark run the bayes, kmeans, and cc benches

--- measure/tachyon/spark/runbench_tachyon.sh
+++ measure/tachyon/spark/runbench_tachyon.sh
@@ -5,7 +5,7 @@
 TACHYON_MASTER=ccied6.sysnet.ucsd.edu
 
 USER=`whoami`
-PREFIX=/user/$USER/data-MicroBenchmarks
+PREFIX=/user/$USER/ICSBigDataBench
 
 TACHYON_PREFIX=tachyon://$TACHYON_MASTER:19998$PREFIX
 
@@ -13,11 +13,18 @@ RAW_FILES=$TACHYON_PREFIX/in
 SORT_FILES=$TACHYON_PREFIX/sort-out
 TSORT_FILE=$TACHYON_PREFIX/tsort-file
 
+BAYES_TESTDATA=$TACHYON_PREFIX/Bayesclassifier/testdata
+BAYES_MODEL=$TACHYON_PREFIX/Bayes/model
+
+KMEANS_DATA=$TACHYON_PREFIX/Spark-Kmeans/data-Kmeans
+
+CC_DATA=$TACHYON_PREFIX/Connect-data/Facebook_genGragh.txt
+
 TACHYON_OUTPUT=$TACHYON_PREFIX/out/spark/tachyon
 
 if [ $# -ne 1 ]
 then
-    echo "please specify the workload to run (pagerank|grep|terasort)'"
+    echo "please specify the workload to run (wordcount|grep|sort|bayes|kmeans|cc)'"
 else
     echo "START RUN "$1
 
@@ -32,7 +39,18 @@ else
     elif [ $1 == "sort" ];then
         $TACHYON_HOME/bin/tachyon tfs rmr $TACHYON_OUTPUT/sort
         $SPARK_HOME/bin/spark-submit --class cn.ac.ict.bigdatabench.Sort bigdatabench-spark_2.10-1.1.0.jar $RAW_FILES $TACHYON_OUTPUT/sort
+    
+    elif [ $1 == "bayes" ];then
+        $TACHYON_HOME/bin/tachyon tfs rmr $TACHYON_OUTPUT/bayes
+        $SPARK_HOME/bin/spark-submit --class cn.ac.ict.bigdatabench.NaiveBayesClassifier bigdatabench-spark_2.10-1.1.0.jar $BAYES_TESTDATA $BAYES_MODEL $TACHYON_OUTPUT/bayes
 
+    elif [ $1 == "kmeans" ];then
+        #$TACHYON_HOME/bin/tachyon tfs rmr $TACHYON_OUTPUT/kmeans
+        $SPARK_HOME/bin/spark-submit --class cn.ac.ict.bigdatabench.KMeans bigdatabench-spark_2.10-1.1.0.jar $KMEANS_DATA 10 10 
+   
+    elif [ $1 == "cc" ];then
+        #$TACHYON_HOME/bin/tachyon tfs rmr $TACHYON_OUTPUT/cc
+        $SPARK_HOME/bin/spark-submit --class cn.ac.ict.bigdatabench.ConnectedComponent bigdatabench-spark_2.10-1.1.0.jar $CC_DATA 
     else
         echo "Sorry we do not support this benchmark ("$WORKLOAD")"
     fi
