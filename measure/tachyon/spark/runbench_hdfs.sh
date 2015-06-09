e9388a625890adcd5241ea886b3edd048b6b8abd
Tianyin Xu <xuty1985@gmail.com>
2015-06-09 14:47:03 -0700

    the same workload on HDFS so we can compare with Tachyon

--- measure/tachyon/spark/runbench_hdfs.sh
+++ measure/tachyon/spark/runbench_hdfs.sh
@@ -11,6 +11,13 @@ RAW_FILES=$HDFS_PREFIX/data-MicroBenchmarks/in
 SORT_FILES=$HDFS_PREFIX/data-MicroBenchmarks/sort-out
 TSORT_FILE=$HDFS_PREFIX/data-MicroBenchmarks/tsort-file
 
+BAYES_TESTDATA=$HDFS_PREFIX/Bayesclassifier/testdata
+BAYES_MODEL=$HDFS_PREFIX/Bayes/model
+
+KMEANS_DATA=$HDFS_PREFIX/Spark-Kmeans/data-Kmeans
+
+CC_DATA=$HDFS_PREFIX/Connect-data/Facebook_genGragh.txt
+
 HDFS_OUTPUT=$HDFS_PREFIX/out/spark/hdfs
 
 if [ $# -ne 1 ]
@@ -30,6 +37,15 @@ else
     elif [ $1 == "sort" ];then
         $HADOOP_HOME/bin/hadoop fs -rm -r $HDFS_OUTPUT/sort
         $SPARK_HOME/bin/spark-submit --class cn.ac.ict.bigdatabench.Sort bigdatabench-spark_2.10-1.1.0.jar $RAW_FILES $HDFS_OUTPUT/sort
+    elif [ $1 == "bayes" ];then
+        $HADOOP_HOME/bin/hadoop fs -rm -r $HDFS_OUTPUT/bayes
+        $SPARK_HOME/bin/spark-submit --class cn.ac.ict.bigdatabench.NaiveBayesClassifier bigdatabench-spark_2.10-1.1.0.jar $BAYES_TESTDATA $BAYES_MODEL $HDFS_OUTPUT/bayes
+
+    elif [ $1 == "kmeans" ];then
+        $SPARK_HOME/bin/spark-submit --class cn.ac.ict.bigdatabench.KMeans bigdatabench-spark_2.10-1.1.0.jar $KMEANS_DATA 10 10 
+   
+    elif [ $1 == "cc" ];then
+        $SPARK_HOME/bin/spark-submit --class cn.ac.ict.bigdatabench.ConnectedComponent bigdatabench-spark_2.10-1.1.0.jar $CC_DATA 
 
     else
         echo "Sorry we do not support this benchmark ("$WORKLOAD")"
